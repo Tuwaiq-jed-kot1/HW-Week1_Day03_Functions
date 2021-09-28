47be58c598973db31231c78c23156c7d48a9970a
Renad Omar <reomqu@gmail.com>
2021-09-29 02:42:32 +0300

    Problem Solved :)

--- src/solution_part1.kt
+++ src/solution_part1.kt
@@ -1,1 +1,17 @@
-fun main(){   }
+fun main()
+{
+    println(add(10,12)) //Q1 function call
+
+    println(fullName("Ahmed","Abdullah","Al Waleed")) //Q2 function call
+}
+
+//Q1 function
+fun add(num1:Int,num2:Int):Int{
+    return num1+num2
+} //end
+
+
+//Q2 function
+fun fullName(firstName: String, middleName:String, lastName:String):String {
+    return "$firstName $middleName $lastName"
+} //end
