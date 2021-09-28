0f05fea67663b7803b7c4deef3ae6e67daf95cd6
asmaa <Asma.Alghamdi.jed.k1@tuwaiq.edu.sa>
2021-09-29 00:47:28 +0300

    the day-3 assignment - Asmaa

--- src/solution_part1.kt
+++ src/solution_part1.kt
@@ -1,1 +1,12 @@
-fun main(){   }
+fun main(){
+    //**the answer of Question 1**
+    println("PART 1: Question 1:")
+    fun add(num1:Int, num2:Int):Int = num1+num2
+    println("${add(10,12)} \n")
+
+    //**the answer of Question 2**
+    println("PART 1: Question 2: ")
+    fun fullName(fName:String,mName:String,lName:String)= "$fName $mName $lName"
+    println(fullName("Ahmed","Abdullah","Al Waleed"))
+
+}
--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,21 @@
 fun main(){
+    //** the answer of question 3**
+    println("PART 2: Question 1:")
+    val maximum :(Int ,Int ,Int)->Int={ n1,n2,n3 -> maxOf(n1,n2,n3)}
+    println("maximum number is ${maximum(10,14,12)} \n")
 
+    // ~~ ** PART 3: the answer of question 1**
+    println("PART 3: Question 1:")
+    fun math(num1:Int,num2:Double,operation:(Int,Double)->Double)=operation(num1,num2)
+    println("Add ${math(2,3.0 ,{num1,num2->(num1+num2)}).toInt()}")
+    println("Subtract: ${math(2,3.0 ,{num1,num2->(num1-num2)}).toInt()}")
+    println("Multiply ${math(2,3.0 ,{num1,num2->(num1*num2)}).toInt()}")
+    println("Divide ${math(2,3.0 ,{num1,num2->(num1/num2)}).toInt()} \n")
+
+    // ~~ ** PART 3: the answer of question 2 **
+    println("PART 3: Question 2: ")
+    val castFireball :(nOfball:Int)->Int={nOfball-> 50-nOfball}
+    val balls= 45
+    if(balls <= 50) println("the remaining fireballs : ${castFireball(balls)}")
+    else println("the maximum number of balls is 50")
 }
