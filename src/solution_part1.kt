ee836f8b9e696fab45c55194f99efa2f75ad0157
faisalha98 <fisal.alharthi.jed.k1@tuwaiq.edu.sa>
2021-09-28 23:39:23 +0300

    The first two questions were easy, the third was hard, and the fourth one I couldn't solve anymore because I'm tired, and I have to study for tomorrow's lesson.

--- src/solution_part1.kt
+++ src/solution_part1.kt
@@ -1,1 +1,17 @@
-fun main(){   }
+fun main() {
+    //Question 1
+    fun add(a: Int, b: Int): Int {
+
+        return a + b
+
+    }
+    println(add(10, 12))
+//Question 2
+
+    fun fullName(n1: String, n2: String, n3: String): String {
+        return n1 + n2 + n3
+    }
+
+    println(fullName(" Ahmed ", " Abdullah ", " Al Waleed"))
+
+}
--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,24 @@
+//Question 3
 fun main(){
+    fun maximum (x:Int, y:Int, z: Int , maximum   : (n1: Int, n2: Int, n3: Int)->Int){
+        println(maximum(10,14,12) )
+        if (x>y){
+            println("The largest number is 10")
 
+        }
+        if (y>x){
+            println("The largest number is 14")
+
+        }
+        if (z>y){
+            println("The largest number is 12")
+
+        }
+
+    }
+    maximum(x = 10, y = 14, z = 12){ n1, n2, n3 ->Int.MAX_VALUE}
 }
+
+
+
+
