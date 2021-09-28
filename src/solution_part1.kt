3dd2e5c6be082b5d6838c4132e32c271f9a966b0
Mahila171 <mhelah161@gmail.com>
2021-09-28 23:04:14 +0300

    Problem Solved :)

--- src/solution_part1.kt
+++ src/solution_part1.kt
@@ -1,1 +1,20 @@
-fun main(){   }
+fun main() {
+
+    println(add(10, 12))
+    println(fullName("Ahmed", "Abdullah", "Al Waleed"))
+
+}
+
+//----------------------------------Question 1------------------------------------------
+// add() is a function that takes two input numbers(num1, num2) and returns the sum of these numbers.
+fun add(num1: Int, num2: Int): Int {
+    return num1 + num2
+
+}
+
+//----------------------------------Question 2------------------------------------------
+// fullName() is a function that takes three inputs strings which are first, middle, and last name, and returns the full name.
+fun fullName(first: String, middle: String, last: String): String {
+    return "$first $middle $last"
+
+}
