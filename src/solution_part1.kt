72582c9c0a78a0c9de60154def3fddab96a710ec
RanaJaberAlrajhi <Rana.Alrajhi.jed.k1@tuwaiq.edu.sa>
2021-09-29 00:45:29 +0300

    question1 and 2 : was good andi did not face any problem

--- src/solution_part1.kt
+++ src/solution_part1.kt
@@ -1,1 +1,20 @@
+//q1
 
+fun main(){
+    fun Add (num1:Int , num2:Int):Int
+    {
+        return num1 +num2
+    }
+    println (Add(22,9))
+}
+
+
+//q2
+
+fun main(){
+    
+    fun fullName(firstName:String,middleName:String,lastName:String):String{
+        return "$firstName $middleName $lastName"
+    }
+    println(fullName("Rana","Jaber","ALrajhi"))
+}
--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,1 +1,22 @@
+//q3
 
+fun main(){
+
+    val maximum= fun(num1:Int , num2:Int , num3:Int):Int{
+        if(num1 >= num2 && num1 >= num3)return num1
+        if(num2 >= num1 && num2 >= num3)return num2
+        return num3
+    }
+    println(maximum(11,17,9))
+}
+
+
+
+//ch1
+
+fun main(){
+    fun math(num1:Int , num2:Int):String{
+        return "Add: "+(num1+num2) + "\nSubtract: "+(num1-num2)+"\nMutiply: "+(num1*num2)+"\nDivide: "+(num1/num2)
+    }
+    println(math(4,5))
+}
