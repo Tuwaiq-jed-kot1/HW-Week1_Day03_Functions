6cc49eab8e689827eec273569e769e22c722ed27
rubaomar <Rr094794003>
2021-09-29 20:14:39 +0300

    i'm late :(

--- src/solution_part1.kt
+++ src/solution_part1.kt
@@ -1,1 +1,12 @@
-fun main(){   }
+// Q1
+//fun main(){
+    //print(add(7,5)) //  حرجع اجرب بعد ما ارسل الواجب
+//}
+//Q2
+
+fun main () {
+    var firstName = "Ruba"
+    var secName = "Omar"
+    var lastName = "Nughyas"
+    println("$firstName $secName $lastName")
+}
--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,13 @@
+// Q3
 fun main(){
+    var fValue = 7
+    var sValue = 70
+    var tValue = 20
+    if (fValue > sValue && fValue > tValue)
+        println(" the largest value is $fValue")
+    else if (sValue > fValue && sValue > tValue)
+        println(" the largest value is $sValue")
+    else if (tValue > sValue && tValue > sValue)
+        println(" the largest value is $tValue") // طولتها وهي قصيرة بس لسا بتعلم :(
 
 }
