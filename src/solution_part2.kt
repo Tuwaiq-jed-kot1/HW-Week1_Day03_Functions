71f9e698dc3b9fae3002ded39670afb58cfcf567
badrFadul <badr.fadul.jed.k1@tuwaiq.edu.sa>
2021-09-29 20:22:18 +0300

    problem solved ^_^

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,4 +1,5 @@
 fun main(){
+
     //q3
     val maximum5 =fun(number1: Int,number2: Int, number3: Int): String{
         val x = number1
@@ -17,8 +18,9 @@ fun main(){
     val largest = maximum5(10,12,14)
     println(largest)
 
-
+    //partII
     //q4
+    //ch1
     fun mathAdd(num1:Int, num2:Int, math0 :(n1:Int, n2:Int)-> Int) {
         val x = num1
         val y = num2
@@ -58,4 +60,25 @@ fun main(){
 
     }
     mathDiv(2,3) { n1, n2 -> n1 / n2 }
+
+
+    //q5
+    //ch2
+    var fuel = 50
+    fun castFireball(numFireballCast: Int):String {
+        val maximum = 50
+        val minimum = 1
+        fuel -= numFireballCast
+        if (fuel == maximum){
+            return "$fuel at it maximum"
+        }else if (fuel in 2..49){
+            return "$fuel is the remaining fuel"
+        }else if (fuel == minimum){
+            return "$fuel at it minimum level"
+        }else{
+            return "out of range"
+        }
+
+    }
+    println(castFireball(49))
 }
