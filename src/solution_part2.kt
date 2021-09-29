f3567a72ebb8d44784874c2a1ef973e940cd288c
Basil19 <bojsl-9@hotmail.com>
2021-09-29 22:29:01 +0300

    Basil alluqmani

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,32 +1,40 @@
+import kotlin.random.Random
 
 fun main() {
 
-    //Question 3
+
+    println("Question 3")
 
 
-    fun max(number1:Int,number2:Int,number3:Int ,mathOperations:(num1:Int,num2:Int,num3:Int)->Int){
+    var number1 = Random.nextInt(0, 100)
+    var number2 = Random.nextInt(0, 100)
+    var number3 = Random.nextInt(0, 100)
+    max(number1, number2, number3, { num1, num2, num3 -> num1 + num2 + num3 })
 
 
-        if (number1>number2){
-            println("The largest number is $number1")
-        }else if (number2>number3){
-
-            println("The largest number is $number2")
-        }else{
-            println("The largest number is $number3")
-        }
+}
 
 
-        println("$number1 + $number2 + $number3 = ${number1 +number2+number3}")
+fun max(number1: Int, number2: Int, number3: Int, mathOperations: (num1: Int, num2: Int, num3: Int) -> Int) {
 
+
+    if (number1 > number2) {
+        println("The largest number is $number1")
+    } else if (number2 > number3) {
+
+        println("The largest number is $number2")
+    } else if (number1 == number2 || number2 == number3 || number1 == number3) {
+        println("There are two numbers equal ")
+    } else {
+
+        println("The largest number is $number3")
 
     }
 
 
-    max(2,21,16, {num1,num2,num3-> num1 +num2+num3})
 
-    
+
+    println("$number1 + $number2 + $number3 = ${number1 + number2 + number3}")
+
+
 }
-
-
-
--- src/solution_part3.kt
+++ src/solution_part3.kt
@@ -4,7 +4,7 @@
 
 //Challenge 1: Mathematical Operationss
 
-fun Mathematical_Operations(number1:Int,number2:Int, mathOperations:(num1:Int,num2:Int)->Int){
+fun Mathematical_Operations(number1:Int,number2:Int,operation:String, mathOperations:(num1:Int,num2:Int)->Int){
 
     println(mathOperations(number1,number2))
 
@@ -13,16 +13,53 @@ fun Mathematical_Operations(number1:Int,number2:Int, mathOperations:(num1:Int,nu
 
 println("Challenge 1: Mathematical Operations")
 
-Mathematical_Operations(2,3,{num1,num2-> num1 + num2})
+Mathematical_Operations(2,3,"Add :",{num1,num2-> num1 + num2})
 
-Mathematical_Operations(2,3,{num1,num2-> num1 - num2})
+Mathematical_Operations(2,3,"Subtract:",{num1,num2-> num1 - num2})
 
-Mathematical_Operations(2,3,{num1,num2-> num1 * num2})
+Mathematical_Operations(2,3,"Multiply :",{num1,num2-> num1 * num2})
 
-Mathematical_Operations(2,3,{num1,num2-> num1 % num2})
+Mathematical_Operations(2,3,"Divide :",{num1,num2-> num1 / num2})
 
 
 
 //Challenge 2: Fireball Fuel Level
+
+
+
+println("please enter fuel between 1 and 50 ")
+
+
+var fuel: Int = Integer.valueOf(readLine())
+
+
+val ball = "fireballs "
+
+if (fuel > 0 && fuel <= 50) {
+
+    for (fuel in fuel downTo 1) {
+
+        println("fireballs $fuel")
+
+    }
+
+    if (fuel != 0) {
+
+        println("The fule end ")
+    }
+
+
+} else if (fuel.equals(null)) {
+    println(" The fule is null")
+
+
+} else {
+    println("the fuel is not between 1 and 50 ")
+
+}
+
+
+
+
 
 
