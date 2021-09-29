c41e99fd24138221f4a17da6dfda770f86074993
Marah <marah.bossi.jed.k1@tuwaiq.edu.sa>
2021-09-29 03:03:00 +0300

    Problem finally Solved :)

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,14 @@
 fun main(){
+    println("The largest number is "+ max(10,14,12))
 
+    //challenge marah albossi
+    fun maths(n1:Int, n2:Int, math:(m1:Int, m2:Int) -> Int){
+        println(math(2, 3))
+    }
+    maths(2,3){m1, m2 -> m1 + m2}
+    maths(2,3){m1, m2 -> m1 - m2}
+    maths(2,3){m1, m2 -> m1 * m2}
+    maths(2,3){m1, m2 -> m1 / m2}
 }
+
+val max = {a:Int, b:Int, c:Int -> maxOf(a, b, c)}
