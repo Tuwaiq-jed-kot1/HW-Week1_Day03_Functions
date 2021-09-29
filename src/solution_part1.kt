1990f92c798b53366d262a0ffd35d33cc4c5af33
Fatim <Fatimah.Alsuhaymi.jed.@tuwaiq.edu.sa>
2021-09-29 08:39:41 +0300

    useful homework

--- src/solution_part1.kt
+++ src/solution_part1.kt
@@ -1,1 +1,17 @@
+fun main(){
+    fun add (x:Int,y:Int):Int {
 
+        return x + y
+    }
+
+    println(add(10,12))
+
+
+    fun fullName(name1:String,name2:String,name3:String):String{
+
+        return name1 +" " +name2 +" " +name3
+    }
+
+    println(fullName("Ahamed","Abdullah","Al walead "))
+
+}
