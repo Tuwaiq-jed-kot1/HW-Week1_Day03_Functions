142c395bd1391804b07059d09a14ecae9a37d1ca
abdullahALshehrii <Abdullah.Alshehri.jed.k1@tuwaiq.edu.sa>
2021-10-10 23:45:32 +0300

    ..

--- src/solution_part1.kt
+++ src/solution_part1.kt
@@ -1,1 +1,7 @@
-
+fun main() {
+    addNumbers(10,12)
+}
+fun addNumbers(num1:Int,num2:Int){
+    println(num1+num2)
+    
+}
--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,1 +1,7 @@
+fun main() {
+    addNames("ahmad","abdullah","al waleed")
+}
 
+fun addNames(nam1:String,nam2:String,nam3:String){
+
+    println("{$nam1 $nam2 $nam3}")
