bd92e0cc5043091a8135f06086b2afde227a86b6
RenadAlqurashi <91476866+RenadAlqurashi@users.noreply.github.com>
2021-11-12 16:57:23 +0300

    Update solution_part2.kt

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -3,4 +3,4 @@ fun main() {
 }
 
 //Q3 Anonymous function
-val maximum: (num1:Int, num2:Int, num3:Int) -> String = {num1,num2,num3 -> "the largest number is " + maxOf(num1,maxOf(num2,num3))} //end
+val maximum: (Int, Int, Int) -> String = {num1, num2, num3 -> "the largest number is " + maxOf(num1,maxOf(num2,num3))} //end
